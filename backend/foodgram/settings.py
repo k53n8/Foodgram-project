679bfcd208199ca354803285cd3003f5b461febf
Олег Иванов <ksengod@yandex.ru>
2023-09-04 11:54:57 +0500

    nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/static/'
+STATIC_ROOT = '/backend_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
