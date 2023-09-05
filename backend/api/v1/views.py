d2f5d10920207210f339e1f05848daf6101324de
Олег Иванов <ksengod@yandex.ru>
2023-09-05 20:58:54 +0500

    download

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -94,7 +94,7 @@ class RecipeViewSet(ModelViewSet):
     def download_shopping_cart(self, request):
         list_of_products = 'Список продуктов:'
         ingredients = IngredientsForRecipes.objects.filter(
-            recipe__in_carts__user=request.user
+            recipe__add_shopcart__user=request.user
         ).order_by('ingredient__name').values(
             'ingredient__name', 'ingredient__measurement_unit'
         ).annotate(amount=F('amount'))
