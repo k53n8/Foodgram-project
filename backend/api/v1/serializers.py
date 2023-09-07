af8be2f1b4a58492dd8c3f9de0a47cc0392c8d1a
Олег Иванов <ksengod@yandex.ru>
2023-09-07 17:23:09 +0500

    please

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -208,7 +208,7 @@ class AmountSerializer(serializers.ModelSerializer):
     """
     id = serializers.PrimaryKeyRelatedField(
         queryset=Ingredient.objects.all(),
-        source='ingredient'
+        source='ingredient.id'
     )
 
     class Meta:
