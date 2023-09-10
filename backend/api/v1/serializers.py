250a2e4eab1a070dff50cddffd86f6a887330f3a
Олег Иванов <ksengod@yandex.ru>
2023-09-10 16:50:44 +0500

    ihghoпва

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -189,7 +189,7 @@ class RecipePostPatchDeleteSerializer(serializers.ModelSerializer):
     def create_bulk_ingredients(self, recipe, ingredients):
         IngredientsForRecipes.objects.bulk_create(
             [IngredientsForRecipes(
-                ingredient=ingredient['ingredient']['id'],
+                ingredient=Ingredient.objects.get(id=ingredient['id']),
                 recipe=recipe,
                 amount=ingredient.get('amount')
             ) for ingredient in ingredients]
