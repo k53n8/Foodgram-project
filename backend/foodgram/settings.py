6ffc12fa59c770fd1ebce73d5857c27bf6c94977
Олег Иванов <ksengod@yandex.ru>
2023-09-12 19:05:24 +0500

    djoser fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -167,7 +167,7 @@ DJOSER = {
         'current_user': 'api.v1.serializers.UserGetSerializer',
     },
     'PERMISSIONS': {
-        'user': ['djoser.permissions.AllowAny'],
+        'user': ['rest_framework.permissions.AllowAny'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
 }
